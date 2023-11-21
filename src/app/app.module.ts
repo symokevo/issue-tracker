2b446f0fe124b01938af81e735ffd999213cd544
aceteasea <symokevo@gmail.com>
2023-11-21 23:30:14 +0300

    create issue-report component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IssueListComponent } from './issue-list/issue-list.component';
+import { IssueReportComponent } from './issue-report/issue-report.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IssueListComponent
+    IssueListComponent,
+    IssueReportComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ClarityModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/issues.service.ts
+++ src/app/issues.service.ts
@@ -5,7 +5,6 @@ import { Issue } from './issue';
   providedIn: 'root'
 })
 export class IssuesService {
-
   private issues: Issue[] = [];
 
   constructor() { }
@@ -13,4 +12,27 @@ export class IssuesService {
   getPendingIssues(): Issue[] {
     return this.issues.filter(issue => !issue.completed);
   }
+
+  createIssue(issue: Issue) {
+    issue.issueNo = this.issues.length + 1;
+    this.issues.push(issue);
+  }
+
+  completeIssue(issue: Issue) {
+    const selectedIssue: Issue = {
+      ...issue,
+      completed: new Date()
+    };
+    const index = this.issues.findIndex(i => i === issue);
+    this.issues[index] = selectedIssue;
+  }
+
+  getSuggestions(title: string): Issue[] {
+    if (title.length > 3) {
+      return this.issues.filter(issue =>
+        issue.title.indexOf(title) !== -1);
+    }
+    return [];
+  }
+
 }
