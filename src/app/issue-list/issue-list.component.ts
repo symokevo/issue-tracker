aec72653744ed9156367f1993075a846cd9bf453
aceteasea <symokevo@gmail.com>
2023-11-21 22:53:39 +0300

    Wire the issue-list component business logic by utilizing the issues service methods

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Issue } from '../issue';
+import { IssuesService } from '../issues.service';
 
 @Component({
   selector: 'app-issue-list',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./issue-list.component.scss']
 })
 export class IssueListComponent {
+  issues: Issue[] = []; 
 
+  // Injecting the IssuesService in the constructor
+  constructor( private issueService: IssuesService ) {};
+
+  // a method to call the getPendingIssues method of the injected service
+  private getIssues() {
+    this.issues = this.issueService.getPendingIssues();
+  }
 }
